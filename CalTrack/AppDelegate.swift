f4b91c563d7ac009a5ab25d91b898f9465cae4bf
farice <farissbahi@gmail.com>
2017-05-27 15:45:14 -0700

    add api key

--- CalTrack/AppDelegate.swift
+++ CalTrack/AppDelegate.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    override init() {
+        super.init()
+        GMSServices.provideAPIKey("AIzaSyBOYBoKP3VQvrD5ddO2q7gZXpwIgD1Bdik")
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
