12f6b216c520290b719121b7f21a3c7cd8e485e1
andrewbihl <andrewbihlva@gmail.com>
2017-05-27 15:21:33 -0700

    Initial Xcode project

---
+++ CalTrack/CalTrack/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CalTrack
+//
+//  Created by Andrew Bihl on 5/27/17.
+//  Copyright © 2017 Andrew Bihl. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CalTrack/CalTrack/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CalTrack
+//
+//  Created by Andrew Bihl on 5/27/17.
+//  Copyright © 2017 Andrew Bihl. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
