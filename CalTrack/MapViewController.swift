3a5e4b2c281a84dd1c983befe31fc8db3baf7d20
farice <farissbahi@gmail.com>
2017-05-27 17:37:40 -0700

    Declare the URL schemes used by the API

--- CalTrack/MapViewController.swift
+++ CalTrack/MapViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapViewController: UIViewController {
 
@@ -21,6 +22,20 @@ class MapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
 
     /*
     // MARK: - Navigation
